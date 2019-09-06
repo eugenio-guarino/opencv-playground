f90299390fc37991b1b479588ab570bbb7172e97
Eugenio Guarino <eugenio.guarino@apetito.co.uk>
2019-09-06 14:11:16 +0100

    refactory directory

--- get_started.py
+++ get_started.py
@@ -10,5 +10,13 @@ cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
 # show image in window
 cv2.imshow('Image', img)
 
+cv2.imwrite("output.jpg", img)
+print(img)
+
+print(img.dtype)
+print(img.shape)
+
 # like console readline
 cv2.waitKey()
+
+
