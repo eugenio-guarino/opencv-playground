3906f5827c301384e9236f0b48422d7c37879850
Eugenio Guarino <eugenio.guarino@apetito.co.uk>
2019-09-17 20:46:14 +0100

    add main method

--- face_recognize.py
+++ face_recognize.py
@@ -1,48 +1,53 @@
 import cv2
 
-# use Haar Cascade object detection algorithm
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-# grab the reference to the webcam
-vs = cv2.VideoCapture(0)
 
-# open file that keeps track of the number of images
-f = open('counter.txt', 'rt+')
-firstLine = f.readline()
-img_counter = int(firstLine)
+def main():
+    # use Haar Cascade object detection algorithm
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+    # grab the reference to the webcam
+    vs = cv2.VideoCapture(0)
 
-while True:
-    # grab the current frame
-    ret, frame = vs.read()
+    # open file that keeps track of the number of images
+    f = open('counter.txt', 'rt+')
+    first_line = f.readline()
+    img_counter = int(first_line)
 
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
-    if frame is None:
-        break
+    while True:
+        # grab the current frame
+        ret, frame = vs.read()
 
-    faces = faceCascade.detectMultiScale(frame, scaleFactor=1.05)
+        # if we are viewing a video and we did not grab a frame,
+        # then we have reached the end of the video
+        if frame is None:
+            break
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        faces = face_cascade.detectMultiScale(frame, scaleFactor=1.05)
 
-    # show the frame to our screen
-    cv2.imshow('Video', frame)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    # if the 'ESC' key is pressed, stop the loop
-    key = cv2.waitKey(1) & 0xFF
-    if key == 27:
-        break
-    # SPACE pressed
-    elif key == 32:
-        img_name = 'facedetect_webcam_{}.png'.format(img_counter)
-        cv2.imwrite(img_name, frame)
-        img_counter += 1
+        # show the frame to our screen
+        cv2.imshow('Video', frame)
 
-        # create a new image n tracker file
-        f.close()
-        f = open('counter.txt', 'w')
-        f.write(f'{img_counter}')
+        # if the 'ESC' key is pressed, stop the loop
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27:
+            break
+        # SPACE pressed
+        elif key == 32:
+            img_name = 'facedetect_webcam_{}.png'.format(img_counter)
+            cv2.imwrite(img_name, frame)
+            img_counter += 1
 
-# close everything
-f.close()
-vs.release()
-cv2.destroyAllWindows()
+            # create a new image n tracker file
+            f.close()
+            f = open('counter.txt', 'w')
+            f.write(f'{img_counter}')
+    # close everything
+    f.close()
+    vs.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
